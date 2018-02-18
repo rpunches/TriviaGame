687d7de1707107ec4c6908e9efa16b39d928e2a7
Rachel <rpunches@gmail.com>
2018-02-17 23:03:51 -0600

    final changes made for homework handin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,11 @@
 
-
+$("#openScreen").show();
+$("#quizScreen").hide();
+$("resBtn").hide();
+var timer = 90;
 function startTimer() {
-    timer = 90;
-
+    
+    $("#quizScreen").show();
     var interval = setInterval(function () {
         timer--;
         $(".timer").text(timer);
@@ -10,13 +13,11 @@ function startTimer() {
     }, 1000);
 }
 
-
-function submitAnswers(e) {
-    e.preventDefault();
+function submitAnswers() {
+    var answers = ["c", "b", "c", "a", "c", "b", "a", "b", "c", "c"];
     var total = 10;
     var score = 0;
 
-    var q0 = document.forms["quoteForm"]["q0"].value;
     var q1 = document.forms["quoteForm"]["q1"].value;
     var q2 = document.forms["quoteForm"]["q2"].value;
     var q3 = document.forms["quoteForm"]["q3"].value;
@@ -26,6 +27,7 @@ function submitAnswers(e) {
     var q7 = document.forms["quoteForm"]["q7"].value;
     var q8 = document.forms["quoteForm"]["q8"].value;
     var q9 = document.forms["quoteForm"]["q9"].value;
+    var q10 = document.forms["quoteForm"]["q10"].value;
 
     for (var i = 1; i <= total; i++) {
         if (eval('q' + i) == null || eval('q' + i) == '') {
@@ -34,58 +36,37 @@ function submitAnswers(e) {
         }
     }
 
-    var answers = ["c", "c", "b", "c", "a", "c", "b", "a", "b", "c"];
-
     for (var i = 1; i <= total; i++) {
         if (eval('q' + i) == answers[i - 1]) {
             score++;
         }
-    }
-    $('#quoteGame').on('submit', function(e){
-        e.preventDefault();
-    });
+    } 
     alert('You scored ' + score + " out of " + total);
+    // $("resBtn").show();
+   
+    // var restartButton = document.createElement("input")
+    //   restartButton.type = "submit";
+    //   restartButton.value = "Try Again!"
+    //   document.body.appendChild(restartButton);
+    // //   $("#openScreen").show();
+    // //   $("#quizScreen").hide();
+    //   restartButton.addEventListener("click", resetGame, true);
 
-    var results = document.getElementById('results');
-    resutls.innerHTML = '<h3> You scored <span' + score + '</span> out of <span>' + total + '</span></h3>';
-    console.log(score);
-
-
-    return false;
 }
 
+function resetGame() {
+    $("#openScreen").show();
+    $("#quizScreen").hide();
+    $("#resBtn").hide();
+    var score = 0;
+    timer = 90;
+    startTimer();
+    
+}
 
-// function getCheckedValue() {
-//     var check = true;
-//     $("input:radio").each(function () {
-//         var name = $(this).attr("name");
-//         if ($("input:radio[name=" + name + "]:checked").length == 0) {
-//             check = false;
+$('#quoteGame').on('submit', function (e) {
+    e.preventDefault();
 
-//         }
-//     });
+});
 
-//     if (check) {
-//         function displayScore() {
-//             for (var i = 0; i < tot; i++) {
-//                 if (getCheckedValue("q" + i) === answers[i]);
-//                 score++;
-//             }
-//         }
-//     }
-// }
-// function results() {
-//     alert("Your score is " + score + "/" + tot);
-//     console.log(score);
-// }
-// function getCheckedValue(radioName) {
-    // var radios = $("input[name='q'+y]:checked").val();
-    // for (var y = 0; y < radios.length; y++)
-    //     if (radios[y].checked) return radios[y].value;
-// };
-
-
-
-
-// $("#submit").on("click", function()
 
